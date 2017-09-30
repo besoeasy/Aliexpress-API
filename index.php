1d8893874ee100391338d9c213586e5acea3b099
Aman Singh <besoeasy@users.noreply.github.com>
2017-09-30 17:48:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <form action=xgo.php method="post">
 URL : <input type="text" name=“url”><br>
-
 <input type="submit">
 </form>
 
 
-
-<br>
-
-
-You Can Also Post Directly To https://aliexpressapi.herokuapp.com/xgo.php  with variable url
-
-<br>
-
-
-<a target=_blank href=besoeasy.com > BESOEASY.com </a>
